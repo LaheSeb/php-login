571a0b11f926cdf0d045ac0036f85d4410c54cac
Seb14 <s.laheyne@laposte.net>
2021-10-06 20:58:05 +0200

    push

--- src/add.php
+++ src/add.php
@@ -1,92 +1,51 @@
-<?php
+<?php  
 
-require_once('manager/UserManager.php');
-require_once('manager/User.php');
- include ('header.php');
-try {
-    $db = new PDO($dsn, $user, $password);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // Si toutes les colonnes sont converties en string
+require_once '../vendor/autoload.php';
 
-   
-    $manager = new UserManager($db);
+use Pacma\PhpLogin\Manager\UserManager;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+$logger = new Logger('main');
 
-    if ($_POST) {
-        if (isset($_POST['email']) && !empty($_POST['email'])) {
+$logger->pushHandler(new StreamHandler(__DIR__. '/../log/app.log', Logger::INFO));
 
-            
-            $email = strip_tags($_POST['email']);
-            $passw = strip_tags($_POST['passw']);
-            $roles = strip_tags($_POST['roles']);
+$logger->info('First message');
+$logger->debug('2eme message');
 
-            
-            $user = new User();
-            $user->setEmail($email);
-            $user->setPassword($passw);
-            $user->setRoles($roles);
-            if ($manager->add($user)) {
+//Definir le dossier ou se trouvent kes templates
+$loader = new FilesystemLoader('..\templates');
+
+// Initialiser l'enviorrement de twig et definir le dossier du cache
+
+$twig = new Environment($loader , ['cache'=> '../cache']);
+
+
+if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['role'])) {
+    try {
+        include "conf.php";
+        $db = new PDO($dsn, $user, $password);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $UserManager = new UserManager($db);
+        $user = new User();
+        if ($Usermanager->add($user)) {
                 
-                $_SESSION['message'] = "VOus avez ajouté un user";
-            } else {
-                
-                $_SESSION['error'] = "ERREUR dan l'ajout";
-            };
-
+            $_SESSION['message'] = "VOus avez ajouté un user";
+        } else {
             
-            header('Location: afficher.php');
-        } 
-    }
-} catch (PDOException $e) {
-    print('<br/>Erreur de connexion : ' . $e->getMessage());
-}
-?>
+            $_SESSION['error'] = "ERREUR dan l'ajout";
+        };
 
-<!DOCTYPE html>
- <html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-     <title>Ajouter un type de livre</title>
- </head>
- <body>
-     <main class="container"> 
-        <div class="row">
         
-            <section class="col-12">
-                <?php
-                    if(!empty($_SESSION['erreur'])){
-                        print('<div class="alert alert-danger" role="alert">'.$_SESSION['erreur'].'</div>');
-                        
-                        $_SESSION['erreur'] = "";
-                        }
-
-                    if(!empty($_SESSION['message'])){
-                            print('<div class="alert alert-danger" role="alert">'.$_SESSION['erreur'].'</div>');
-                            
-                        $_SESSION['messgae'] = "";
-                        }
-
-                ?>
-                <h1>Ajouter un utilisateur</h1>
-
-                <form method="POST">
-                    <div class="mb-3">
-                        <label>Email : </label>
-                        <input type ="text" id="email" name="email" class="form-control" placeholder="Rentrez votre adresse email....">
-                        <br>
-                        <label>Mot de pass : </label>
-                        <input type ="password" id="passw" name="passw" class="form-control" placeholder="Tapez votre mot de passe....">
-                        <br>
-                        <label>Roles de l'utilisateur  : </label>
-                        <input type ="text" id="roles" name="roles" class="form-control" placeholder="admin">
-                    </div>
-                        <button class="btn btn-primary">Enregistrer</button>
-                        <a class ="btn btn-info"href="index.php"> Retour à la liste</a>
-                </form>
-            </session>
-        </div>
-
-    </main>
- </body>
- </html>
+        header('Location: afficher.php');
+    } catch (PDOException $e) {
+        print('<br/>Erreur de connexion : ' . $e->getMessage());
+    }
+}
+echo $twig->render('add.html.twig', [
+    'title' => 'Ajouter utilisateur', 
+    'user' => $user,
+    
+]);
+?>
--- src/index.php
+++ src/index.php
@@ -1,8 +1,9 @@
 <?php  
-include ('conf.php');
+
 require_once '../vendor/autoload.php';
 
-use App\Manager\UserManager;
+use Pacma\PhpLogin\Manager\UserManager;
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Twig\Loader\FilesystemLoader;
@@ -14,25 +15,26 @@ $logger->pushHandler(new StreamHandler(__DIR__. '/../log/app.log', Logger::INFO)
 $logger->info('First message');
 $logger->debug('2eme message');
 
-print("1) OK <br/>");
+print("OK");
 
-//Definir le dossier ou se trouvent kes templates
-$loader = new FilesystemLoader('..\templates');
+$loader = new FilesystemLoader('../templates');
+$twig = new Environment($loader, ['cache' => '../cache']);
 
-print("2) OK<br/>");
+try {
+    include "conf.php";
+    $db = new PDO($dsn, $user, $password);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $userManager = new UserManager($db);
+    $users = $userManager->getList();
 
 
-// Initialiser l'enviorrement de twig et definir le dossier du cache
 
-$twig = new Environment($loader , ['cache'=> '../cache']);
-
-print("3) OK<br/>");
-$db = new PDO($dsn, $user, $password);
-  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); 
-  $manager = new UserManager($db);
-
-echo $twig->render('index.html.twig',
-[
-    'title' => 'Liste des utilisateur',
-    'users'  => $manager->getList(),
+} catch (PDOException $e) {
+    print('<br/>Erreur de connexion : ' . $e->getMessage());
+}
+echo $twig->render('index.html.twig', [
+    'title' => 'Liste des utilisateurs', 
+    'users' => $users,
 ]);
+
+?>
